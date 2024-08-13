b1e36bf58053d40deda7f1648761a56aa9fae5a6
Md. Nourose Naim Rhedoy <mdnourosenaimrhedoy@gmail.com>
2024-08-13 10:02:23 +0600

    feat: tailwindCSS by CDN

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,24 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
+    <style type="text/tailwindcss">
+        @layer utilities {
+            .container{
+                @apply px-10 mx-auto;
+                content-visibility: auto;
+            }
+        }
+    </style>
     <title>Document</title>
 </head>
 <body>
-    hello Nourose
+    <div class="container">
+            <div class="flex justify-between my-5 ">
+                <h2 class="text-red-500 text-xl">Hello Nourose</h2>
+                <a href="/create" class="bg-green-600 text-white rounded py-2px px-4px">Add New Post</a>
+            </div>
+    </div>
+
 </body>
 </html>
